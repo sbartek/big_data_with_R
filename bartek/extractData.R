412fceac93266e290cc51e99c7010e1240e27e44
Bartek Skorulski <bartekskorulski@gmail.com>
2017-06-27 22:34:29 +0200

    test

--- bartek/extractData.R
+++ bartek/extractData.R
@@ -1,5 +1,4 @@
 source('configuration.R')
-
 zip_files <- list.files(path = INSTACART_DATA_DIR, pattern = "*.zip")
 for (ifile in zip_files) {
   unzip(file.path(INSTACART_DATA_DIR, ifile), exdir = DATA_DIR)
