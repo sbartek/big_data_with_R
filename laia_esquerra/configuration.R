5faa30b4609bf45931c30ef079c021807367c5ca
laiaesquerra <laia.esquerra@gmail.com>
2017-06-28 09:49:54 +0200

    files day 1

---
+++ laia_esquerra/configuration.R
@@ -0,0 +1,6 @@
+#Cal comprovar el directori inicialment, volem estar a la nostra carpeta
+# ".." pugen un nivell (des del directori) a buscar els fitxers
+DATA_DIR  <- file.path("..", "data")
+INSTACART_DATA_DIR <- file.path("..", "instacart_data")
+FOOTBALL_DATA_DIR <- file.path("..", "football_data")
+
